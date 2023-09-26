a2a26a71b0432d2cbdbf23afc82d67ffee7efbd8
Antoine Laffargue <antoine.laffargue@gmail.com>
2023-09-26 14:06:21 +0200

    fix: align items to bottom of grids

--- packages/forms/src/components/FormLayout/index.tsx
+++ packages/forms/src/components/FormLayout/index.tsx
@@ -17,80 +17,97 @@ import {
 } from '../../types'
 import { MAX_SIZE_RESOLUTION } from '../..'
 
-const SIZES_CLASSNAMES: {
+const GRID_SIZES_CLASSNAMES: {
+  [k in FormItemSizeNumber]: string
+} = {
+  [1]: 'grid-cols-1 items-end',
+  [2]: 'grid-cols-2 items-end',
+  [3]: 'grid-cols-3 items-end',
+  [4]: 'grid-cols-4 items-end',
+  [5]: 'grid-cols-5 items-end',
+  [6]: 'grid-cols-6 items-end',
+  [7]: 'grid-cols-7 items-end',
+  [8]: 'grid-cols-8 items-end',
+  [9]: 'grid-cols-9 items-end',
+  [10]: 'grid-cols-10 items-end',
+  [11]: 'grid-cols-11 items-end',
+  [12]: 'grid-cols-12 items-end',
+}
+
+const ITEMS_SIZES_CLASSNAMES: {
   [k in FormItemSizeBreakpoint]: {
-    [k in FormItemSizeNumber]: { grid: string; item: string }
+    [k in FormItemSizeNumber]: string
   }
 } = {
   default: {
-    [1]: { grid: 'grid-cols-1', item: 'col-span-1' },
-    [2]: { grid: 'grid-cols-2', item: 'col-span-2' },
-    [3]: { grid: 'grid-cols-3', item: 'col-span-3' },
-    [4]: { grid: 'grid-cols-4', item: 'col-span-4' },
-    [5]: { grid: 'grid-cols-5', item: 'col-span-5' },
-    [6]: { grid: 'grid-cols-6', item: 'col-span-6' },
-    [7]: { grid: 'grid-cols-7', item: 'col-span-7' },
-    [8]: { grid: 'grid-cols-8', item: 'col-span-8' },
-    [9]: { grid: 'grid-cols-9', item: 'col-span-9' },
-    [10]: { grid: 'grid-cols-10', item: 'col-span-10' },
-    [11]: { grid: 'grid-cols-11', item: 'col-span-11' },
-    [12]: { grid: 'grid-cols-12', item: 'col-span-12' },
+    [1]: 'col-span-1',
+    [2]: 'col-span-2',
+    [3]: 'col-span-3',
+    [4]: 'col-span-4',
+    [5]: 'col-span-5',
+    [6]: 'col-span-6',
+    [7]: 'col-span-7',
+    [8]: 'col-span-8',
+    [9]: 'col-span-9',
+    [10]: 'col-span-10',
+    [11]: 'col-span-11',
+    [12]: 'col-span-12',
   },
   sm: {
-    [1]: { grid: 'grid-cols-1', item: 'sm:col-span-1' },
-    [2]: { grid: 'grid-cols-2', item: 'sm:col-span-2' },
-    [3]: { grid: 'grid-cols-3', item: 'sm:col-span-3' },
-    [4]: { grid: 'grid-cols-4', item: 'sm:col-span-4' },
-    [5]: { grid: 'grid-cols-5', item: 'sm:col-span-5' },
-    [6]: { grid: 'grid-cols-6', item: 'sm:col-span-6' },
-    [7]: { grid: 'grid-cols-7', item: 'sm:col-span-7' },
-    [8]: { grid: 'grid-cols-8', item: 'sm:col-span-8' },
-    [9]: { grid: 'grid-cols-9', item: 'sm:col-span-9' },
-    [10]: { grid: 'grid-cols-10', item: 'sm:col-span-10' },
-    [11]: { grid: 'grid-cols-11', item: 'sm:col-span-11' },
-    [12]: { grid: 'grid-cols-12', item: 'sm:col-span-12' },
+    [1]: 'sm:col-span-1',
+    [2]: 'sm:col-span-2',
+    [3]: 'sm:col-span-3',
+    [4]: 'sm:col-span-4',
+    [5]: 'sm:col-span-5',
+    [6]: 'sm:col-span-6',
+    [7]: 'sm:col-span-7',
+    [8]: 'sm:col-span-8',
+    [9]: 'sm:col-span-9',
+    [10]: 'sm:col-span-10',
+    [11]: 'sm:col-span-11',
+    [12]: 'sm:col-span-12',
   },
   md: {
-    [1]: { grid: 'grid-cols-1', item: 'md:col-span-1' },
-    [2]: { grid: 'grid-cols-2', item: 'md:col-span-2' },
-    [3]: { grid: 'grid-cols-3', item: 'md:col-span-3' },
-    [4]: { grid: 'grid-cols-4', item: 'md:col-span-4' },
-    [5]: { grid: 'grid-cols-5', item: 'md:col-span-5' },
-    [6]: { grid: 'grid-cols-6', item: 'md:col-span-6' },
-    [7]: { grid: 'grid-cols-7', item: 'md:col-span-7' },
-    [8]: { grid: 'grid-cols-8', item: 'md:col-span-8' },
-    [9]: { grid: 'grid-cols-9', item: 'md:col-span-9' },
-    [10]: { grid: 'grid-cols-10', item: 'md:col-span-10' },
-    [11]: { grid: 'grid-cols-11', item: 'md:col-span-11' },
-    [12]: { grid: 'grid-cols-12', item: 'md:col-span-12' },
+    [1]: 'md:col-span-1',
+    [2]: 'md:col-span-2',
+    [3]: 'md:col-span-3',
+    [4]: 'md:col-span-4',
+    [5]: 'md:col-span-5',
+    [6]: 'md:col-span-6',
+    [7]: 'md:col-span-7',
+    [8]: 'md:col-span-8',
+    [9]: 'md:col-span-9',
+    [10]: 'md:col-span-10',
+    [11]: 'md:col-span-11',
+    [12]: 'md:col-span-12',
   },
   lg: {
-    [1]: { grid: 'grid-cols-1', item: 'lg:col-span-1' },
-    [2]: { grid: 'grid-cols-2', item: 'lg:col-span-2' },
-    [3]: { grid: 'grid-cols-3', item: 'lg:col-span-3' },
-    [4]: { grid: 'grid-cols-4', item: 'lg:col-span-4' },
-    [5]: { grid: 'grid-cols-5', item: 'lg:col-span-5' },
-    [6]: { grid: 'grid-cols-6', item: 'lg:col-span-6' },
-    [7]: { grid: 'grid-cols-7', item: 'lg:col-span-7' },
-    [8]: { grid: 'grid-cols-8', item: 'lg:col-span-8' },
-    [9]: { grid: 'grid-cols-9', item: 'lg:col-span-9' },
-    [10]: { grid: 'grid-cols-10', item: 'lg:col-span-10' },
-    [11]: { grid: 'grid-cols-11', item: 'lg:col-span-11' },
-    [12]: { grid: 'grid-cols-12', item: 'lg:col-span-12' },
+    [1]: 'lg:col-span-1',
+    [2]: 'lg:col-span-2',
+    [3]: 'lg:col-span-3',
+    [4]: 'lg:col-span-4',
+    [5]: 'lg:col-span-5',
+    [6]: 'lg:col-span-6',
+    [7]: 'lg:col-span-7',
+    [8]: 'lg:col-span-8',
+    [9]: 'lg:col-span-9',
+    [10]: 'lg:col-span-10',
+    [11]: 'lg:col-span-11',
+    [12]: 'lg:col-span-12',
   },
   xl: {
-    [1]: { grid: 'grid-cols-1', item: 'xl:col-span-1' },
-    [2]: { grid: 'grid-cols-2', item: 'xl:col-span-2' },
-    [3]: { grid: 'grid-cols-3', item: 'xl:col-span-3' },
-    [4]: { grid: 'grid-cols-4', item: 'xl:col-span-4' },
-    [5]: { grid: 'grid-cols-5', item: 'xl:col-span-5' },
-    [6]: { grid: 'grid-cols-6', item: 'xl:col-span-6' },
-    [7]: { grid: 'grid-cols-7', item: 'xl:col-span-7' },
-    [8]: { grid: 'grid-cols-8', item: 'xl:col-span-8' },
-    [9]: { grid: 'grid-cols-9', item: 'xl:col-span-9' },
-    [10]: { grid: 'grid-cols-10', item: 'xl:col-span-10' },
-    [11]: { grid: 'grid-cols-11', item: 'xl:col-span-11' },
-    [12]: { grid: 'grid-cols-12', item: 'xl:col-span-12' },
+    [1]: 'xl:col-span-1',
+    [2]: 'xl:col-span-2',
+    [3]: 'xl:col-span-3',
+    [4]: 'xl:col-span-4',
+    [5]: 'xl:col-span-5',
+    [6]: 'xl:col-span-6',
+    [7]: 'xl:col-span-7',
+    [8]: 'xl:col-span-8',
+    [9]: 'xl:col-span-9',
+    [10]: 'xl:col-span-10',
+    [11]: 'xl:col-span-11',
+    [12]: 'xl:col-span-12',
   },
 }
 
@@ -101,7 +118,7 @@ export const FormLayout: FormRenderer = ({
   className = '',
   children: customFormItemRenderer,
 }) => {
-  const gridSizeClassName = SIZES_CLASSNAMES.sm[MAX_SIZE_RESOLUTION].grid
+  const gridSizeClassName = GRID_SIZES_CLASSNAMES[MAX_SIZE_RESOLUTION]
 
   return (
     <div className={`grid ${gridSizeClassName} gap-3 ${className}`}>
@@ -216,7 +233,7 @@ const getItemSizeClassName = ({ layout }: FormItem) => {
   return Object.entries(size).reduce(
     (className, [breakpoint, size]) =>
       `${className} ${
-        SIZES_CLASSNAMES[breakpoint as FormItemSizeBreakpoint][size].item
+        ITEMS_SIZES_CLASSNAMES[breakpoint as FormItemSizeBreakpoint][size]
       }`,
     '',
   )
